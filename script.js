a77c90aff86b151a196f8c7c916b79811938e6e4
odko2000 <69565961+odko2000@users.noreply.github.com>
2021-06-08 13:47:55 +0800

    Initial commit

---
+++ script.js
@@ -0,0 +1,64 @@
+const output = document.querySelector(".output");
+const number = document.querySelectorAll(".number");
+const operator = document.querySelectorAll(".operator");
+const equalSign = document.querySelector(".equal");
+const clearSign = document.querySelector(".all-clear");
+const decimal = document.querySelector(".decimal");
+var numbers = [];
+var operators = [];
+
+for (var i = 0; i < number.length; i++) {
+  number[i].addEventListener("click", function () {
+    if (output.value === "0") output.value = this.value;
+    else output.value += this.value;
+  });
+}
+
+decimal.onclick = () => {
+  if (output.value.includes(".")) return;
+  output.value += decimal.value;
+};
+
+for (let i = 0; i < operator.length; i++) {
+  operator[i].addEventListener("click", function () {
+    numbers.push(parseFloat(output.value));
+    output.value = "";
+    operators.push(this.value);
+  });
+}
+
+function clearArrays() {
+  numbers = [];
+  operators = [];
+}
+
+equalSign.addEventListener("click", function () {
+  console.log(operators);
+  console.log(numbers);
+  for (let i = 0, j = 0; i < numbers.length, j < operators.length; i++, j++) {
+    if (operators[j] === "×") {
+      output.value = numbers[0] * numbers[1];
+    }
+    if (operators[j] === "÷") {
+      output.value = numbers[0] / numbers[1];
+    }
+    if (operators[j] === "+") {
+      output.value = numbers[0] + numbers[1];
+    }
+    if (operators[j] === "-") {
+      output.value = numbers[0] - numbers[1];
+    }
+    console.log(operators);
+    console.log(numbers);
+    numbers.push(parseFloat(output.value));
+    numbers.shift();
+    numbers.shift();
+    numbers.reverse();
+  }
+  clearArrays();
+});
+
+clearSign.onclick = () => {
+  output.value = "0";
+  clearArrays();
+};
